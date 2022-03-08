45652c210cbf52ea1c39e9fd8320b08e27a4d9f7
rudghska <rudghska@gmail.com>
2022-03-08 17:31:21 +0900

    TodoItem component Delete add

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import styles from './todoItem.module.css';
+import { todoState } from '../../atoms/todoAtom';
 import { Todos } from '../../types/todos';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 
 type TodoProps = {
   todo: Todos;
@@ -8,12 +9,29 @@ type TodoProps = {
 };
 
 const TodoItem = ({ todo }: TodoProps) => {
-  const deleteTodo = () => {};
-  const changeTodoState = () => {};
+  const currentTodos = useRecoilValue<Todos[]>(todoState);
+  const todoHandler = useSetRecoilState<Todos[]>(todoState);
+
+  const deleteTodo = () => {
+    const todos = currentTodos.filter(currentTodo => {
+      if (currentTodo.id !== todo.id) return currentTodo;
+    });
+    todoHandler(() => [...todos]);
+  };
+
+  const checkTodoState = () => {
+    const todos = currentTodos.map(currentTodo => {
+      if (currentTodo.id === todo.id) {
+        return { ...currentTodo, isDone: !todo.isDone };
+      }
+      return currentTodo;
+    });
+    todoHandler(() => [...todos]);
+  };
 
   return (
     <li className={styles.todo}>
-      <input className={styles.checkbox} type="checkbox" onChange={changeTodoState} />
+      <input className={styles.checkbox} type="checkbox" onChange={checkTodoState} />
       <span className={styles.todoName}>{todo.todoName}</span>
       <button onClick={deleteTodo} className={styles.button}>
         <i className="fas fa-trash"></i>
