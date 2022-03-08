7927c6263820807239e1126a410cf9eb47459713
rudghska <rudghska@gmail.com>
2022-03-08 17:32:19 +0900

    atom todo 총 갯수 selector add

--- src/atoms/todoAtom.ts
+++ src/atoms/todoAtom.ts
@@ -1,7 +1,16 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 import { Todos } from './../types/todos';
 
 export const todoState = atom<Todos[]>({
   key: 'todos',
   default: [],
+});
+
+export const todoCountSelector = selector({
+  key: 'todoCount',
+  get: ({ get }) => {
+    const todos = get(todoState);
+
+    return `해야 할 일 : ${todos.filter(todo => !todo.isDone).length}`;
+  },
 });
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './header.module.css';
+import { useRecoilValue } from 'recoil';
 import { Todos } from '../../types/todos';
 
+import { todoCountSelector } from '../../atoms/todoAtom';
+
 const Header = () => {
+  const resultCount = useRecoilValue(todoCountSelector);
+
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>To Do List</h1>
-      <p className={styles.howMany}>{}</p>
+      <p className={styles.howMany}>{resultCount}</p>
     </header>
   );
 };
